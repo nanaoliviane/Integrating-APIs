57b4b9320c3be167dd7aefd6f52f2556d2fe44db
Nana Olivia <139480373+nanaoliviane@users.noreply.github.com>
2024-04-27 11:35:38 +0000

    rendered changes to my views and urls

--- django_project/settings.py
+++ django_project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
