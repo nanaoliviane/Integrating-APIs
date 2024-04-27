044eb7938310eb0cef814f22d201bb663a975d41
Nana Olivia <139480373+nanaoliviane@users.noreply.github.com>
2024-04-27 16:17:10 +0000

    took character api to form magic in my blog

--- django_project/views.py
+++ django_project/views.py
@@ -1,4 +1,11 @@
+import requests
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'templates/index.html')
+  response = requests.get("https://swapi-api.alx-tools.com/api/films/7/")
+  film_data = response.json()
+  return render(request, 'templates/index.html', {'film_data': film_data})
+    
+  
+  
